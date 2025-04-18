19892d84e194783b32175051e18f2834f991ce81
Muhammad_Rafay_Aslam <rafayaslam2311@gmail.com>
2025-04-18 11:11:48 +0500

    updated code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBXUWaGH4tCeFTV8nr6kecC17MDXVXAcF8",
@@ -13,6 +14,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
@@ -139,3 +141,119 @@ getuserLogoutBtn.addEventListener('click', () => {
 })
 
 let span = document.getElementById('user-email')
+let getName = document.getElementById('name')
+let getPrice = document.getElementById('price')
+let getdiscription = document.getElementById('dis')
+let getImgUrl = document.getElementById('img-url')
+let getCards = document.getElementById('cards')
+let getsaveDataBtn = document.getElementById('savedata')
+
+getsaveDataBtn.addEventListener('click', async () => {
+  getCards.innerHTML = ""
+  try {
+    const docRef = await addDoc(collection(db, "dishes"), {
+      name: getName.value,
+      price: getPrice.value,
+      discription: getdiscription.value,
+      image: getImgUrl.value
+    });
+    Swal.fire({
+      title: "Do you want to save the changes?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Save",
+      denyButtonText: `Don't save`
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Saved!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
+    readData()
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+
+})
+
+async function readData() {
+  const querySnapshot = await getDocs(collection(db, "dishes"));
+  querySnapshot.forEach((doc) => {
+    getCards.innerHTML += ` <div class="card m-2 pt-2" style="width: 18rem;">
+  <img src="${doc.data().image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${doc.data().name}</h5>
+    <h6 class="card-title">RS : ${doc.data().price}</h6>
+    <p class="card-text">${doc.data().discription}</p>
+    <a href="#" class="btn btn-primary"    onclick="openEditModal('${doc.id}', '${doc.data().name}', '${doc.data().price}', '${doc.data().discription}', '${doc.data().image}')">Edit</a>
+    <a href="#" class="btn btn-danger" onclick="delItem('${doc.id}')">Delete</a>
+  </div>
+  </div>`
+    // console.log(`${doc.id} => ${doc.data()}`);
+  });
+
+}
+readData()
+
+async function delItem(id) {
+  getCards.innerHTML = "";
+  const cityRef = doc(db, "dishes", id);
+  await deleteDoc(cityRef, {
+    capital: deleteField(),
+  });
+  readData();
+}
+window.delItem = delItem;
+let editingProductId = null;
+window.openEditModal = function ( name, price, discription, image) {
+  editingProductId = id;
+  document.getElementById("editProductName").value = name;
+  document.getElementById("editProductPrice").value = price;
+  document.getElementById("editProductDesc").value = discription;
+  document.getElementById("editProductImage").value = image;
+
+  let editModal = new bootstrap.Modal(
+    document.getElementById("editProductModal")
+  );
+  editModal.show();
+};
+
+window.saveProductChanges = async function () {
+  const name = document.getElementById("editProductName").value;
+  const price = document.getElementById("editProductPrice").value;
+  const discription = document.getElementById("editProductDesc").value;
+  const image = document.getElementById("editProductImage").value;
+
+  const productRef = doc(db, "dishes", editingProductId);
+
+  try {
+    await updateDoc(productRef, {
+      name,
+      price,
+      discription,
+      image,
+    });
+
+    Swal.fire({
+      title: "Updated!",
+      text: "Product updated successfully.",
+      icon: "success",
+    });
+
+    getCards.innerHTML = "";
+    readData();
+
+    bootstrap.Modal.getInstance(
+      document.getElementById("editProductModal")
+    ).hide();
+  } catch (error) {
+    Swal.fire({
+      icon: "error",
+      title: "Update Failed",
+      text: error.message,
+    });
+  }
+};
+  
